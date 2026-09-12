f762499e2b1d965ec6b469631684cf1d9cf40233
Khalitus <h.khalitusali@gmail.com>
2026-09-13 00:49:45 +0500

    feat(vector): refined printing of n results of semantic search

--- property-intelligence-engine/main.py
+++ property-intelligence-engine/main.py
@@ -155,8 +155,15 @@ def main():
 
     print("\nSemantic search")
     print("-" * 32)
-    print(results)
-
+    
+    for document, metadata, distance in zip(
+        results["documents"][0],
+        results["metadatas"][0],
+        results["distances"][0],
+    ):
+        print(f"\nDistance: {distance:.4f}")
+        print(metadata)
+        print(document)
 
 if __name__ == "__main__":
     main()
